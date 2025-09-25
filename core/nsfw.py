f0d3d14a955ae96dfd7684bc31b818bddb48c5fb
welnecker <janiofdr@gmail.com>
2025-09-25 08:22:47 -0300

    Update nsfw.py

--- core/nsfw.py
+++ core/nsfw.py
@@ -1,1 +1,7 @@
+from .repositories import get_fact, last_event
 
+def nsfw_enabled(usuario: str) -> bool:
+    virgem = bool(get_fact(usuario, "virgem", True))
+    if not virgem:
+        return True
+    return bool(last_event(usuario, "primeira_vez"))
